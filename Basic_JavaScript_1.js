5e2b96c60404fd18b63601c1325d697d1d498986
Nick-1881 <nkirby0222@gmail.com>
2022-09-28 12:09:15 -0600

    added web files

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,20 @@
+
+
+
+
+function My_First_Function() {
+var string = "NewYork City population 2020: 8.38 million";
+var result = string.fontcolor ("red");
+document.getElementById("newyork").innerHTML = result;
+}
+
+
+function My_First_Function_1() {
+var string = "chicago population 2020: 2.699 million";
+var result = string.fontcolor ("red");
+document.getElementById("Chicago").innerHTML = result;
+}
+
+
+
+
